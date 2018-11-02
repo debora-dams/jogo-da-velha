00d5ed8ca6bbafa266e3fa977b34059dbb6f4057
debora-dams <deboraaurea7@gmail.com>
2018-11-02 11:02:14 -0300

    modificação da classe Jogo

--- src/jogoDaVelha/Jogo.java
+++ src/jogoDaVelha/Jogo.java
@@ -9,75 +9,102 @@ public class Jogo {
 	private Jogador jogador1;
 	private Jogador jogador2;
 	public Scanner entrada = new Scanner(System.in);
-	
+
 	public Jogo() {
 		tabuleiro = new Tabuleiro();
 		iniciarJogadores();
-		
-		while(Jogar());
+
+		while (jogar())
+			;
 	}
-	
+
 	public void iniciarJogadores() {
 		System.out.println("Quem sera o Jogador 1?");
-		if(escolherJogador() == 1)
+		if (escolherJogador() == 1)
 			this.jogador1 = new Humano(1);
-		
+
 		else
 			this.jogador1 = new Computador(1);
-		
+
 		System.out.println("-----------------------");
 		System.out.println("Quem sera o Jogador 2?");
-		
-		if(escolherJogador() == 1)
+
+		if (escolherJogador() == 1)
 			this.jogador2 = new Humano(2);
 		else
 			this.jogador2 = new Computador(2);
 	}
-	
+
 	public int escolherJogador() {
 		int opcao = 0;
-		
+
 		do {
 			System.out.println("1 - Humano");
 			System.out.println("2 - Computador\n");
 			System.out.println("Opção: ");
 			opcao = entrada.nextInt();
-			
-			if((opcao != 1) && (opcao != 2))
+
+			if ((opcao != 1) && (opcao != 2))
 				System.out.println("Opcao invalida! Tente novamente");
-		}
-		while((opcao != 1) && (opcao != 2));
-		
+		} while ((opcao != 1) && (opcao != 2));
+
 		return opcao;
 	}
-	
+
 	public boolean jogar() {
-		if(ganhou() == 0) {
+		if (ganhou() == 0) {
 			System.out.println("-------------------");
 			System.out.println("\nRodada " + rodada);
 			System.out.println("É a vez d jogador " + vez());
-			
-			if(vez() == 1)
+
+			if (vez() == 1)
 				jogador1.jogar(tabuleiro);
 			else
 				jogador2.jogar(tabuleiro);
-			
-			if(tabuleiro.tabuleiroCompleto()) {
+
+			if (tabuleiro.tabuleiroCompleto()) {
 				System.out.println("Tabuleiro Completo. Jogo empatado");
 				return false;
 			}
-			
+
 			vez++;
 			rodada++;
-			
+
 			return true;
-		}
-		else {
-			if(ganhou() == -1)
+		} else {
+			if (ganhou() == -1)
 				System.out.println("Jogador 1 ganhou!!!");
 			else
 				System.out.println("Jogador 2 ganhou!!!");
+
+			return false;
 		}
 	}
-	
+
+	public int vez() {
+		if (vez % 2 == 1)
+			return 1;
+
+		else
+			return 2;
+	}
+
+	public int ganhou() {
+		if (tabuleiro.checarLinhas() == 1)
+			return 1;
+		if (tabuleiro.checarColunas() == 1)
+			return 1;
+		if (tabuleiro.checarDiagonais() == 1)
+			return 1;
+
+		if (tabuleiro.checarLinhas() == -1)
+			return -1;
+		if (tabuleiro.checarColunas() == -1)
+			return -1;
+		if (tabuleiro.checarDiagonais() == -1)
+			return -1;
+
+		return 0;
+	}
+
 }
