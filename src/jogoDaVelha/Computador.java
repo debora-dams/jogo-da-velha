1d37b3c6890362757edb6075852369a98f287143
debora-dams <deboraaurea7@gmail.com>
2018-11-02 11:03:38 -0300

    adição da classe Computador

---
+++ src/jogoDaVelha/Computador.java
@@ -0,0 +1,20 @@
+package jogoDaVelha;
+
+public class Computador extends Jogador {
+
+	public Computador(int jogador) {
+		super(jogador);
+		System.out.println("Jogador 'Computador' entrou no jogo");
+	}
+
+	@Override
+	public void jogar(Tabuleiro tabuleiro) {
+
+	}
+
+	@Override
+	public void tentativa(Tabuleiro tabuleiro) {
+
+	}
+
+}
